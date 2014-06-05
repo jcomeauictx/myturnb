ba483ed071ef237dee1493e8ecabd940667f826f
Abraham Lopez <abraham.lopez@aplimovil.mx>
2014-06-05 15:02:17 -0500

    tweaks and fixes to app

--- public/app/util/UrlUtils.js
+++ public/app/util/UrlUtils.js
@@ -2,7 +2,7 @@ Ext.define('testing.util.UrlUtils', {
 	singleton: true,
 	getBaseUrl: function() {
 		var isNative = EnvUtils.isNative();
-		var url = isNative ? 'http://mturn.hp.af.cm/' : '';
+		var url = isNative ? 'http://mturn.hp.af.cm/' : 'http://localhost:3000/';
 		return url;
 	}
 });
