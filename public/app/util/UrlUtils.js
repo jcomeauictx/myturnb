e5130a33ee098e2a9456dbc4b5f5d80db4bcad0a
Abraham Lopez <abraham.lopez@aplimovil.mx>
2014-07-02 15:39:54 -0500

    amend to previous commit

--- public/app/util/UrlUtils.js
+++ public/app/util/UrlUtils.js
@@ -2,7 +2,7 @@ Ext.define('testing.util.UrlUtils', {
 	singleton: true,
 	getBaseUrl: function() {
 		var isNative = EnvUtils.isNative();
-		var url = isNative || (location.href.indexOf('http://localhost/') != -1) ? 'http://aplimovil.net:3000/' : 'http://localhost:3000/';
+		var url = isNative || (location.href.indexOf('http://localhost/') == -1) ? 'http://aplimovil.net:3000/' : 'http://localhost:3000/';
 		return url;
 	}
 });
