07dee8ee35f690c1e6ea9e2d1bede0b1f37cc165
John Comeau <jc@unternet.net>
2017-04-06 21:19:00 -0700

    added debugging messages, and set url to the base URL except for the one case of http://localhost/ which becomes http://localhost:3000/

--- public/app/util/EnvUtils.js
+++ public/app/util/EnvUtils.js
@@ -3,6 +3,7 @@ Ext.define('testing.util.EnvUtils', {
 	singleton: true,
 	isNative: function() {
 		var result = typeof(device) !== 'undefined' && device && device.name;
+		if (result) console.log("isNative() set to true for " + device.name);
 		return result;
 	}
 });
--- public/app/util/UrlUtils.js
+++ public/app/util/UrlUtils.js
@@ -1,8 +1,14 @@
 Ext.define('testing.util.UrlUtils', {
+	/* I don't know the purpose of this routine, but it was hard-coding
+	 * the URL to http://myturn.mobi/ and this was causing problems
+	 * with my testing on other servers. So I changed it. Hopefully
+	 * I haven't broken some needed functionality -- jc@unternet.net */
 	singleton: true,
 	getBaseUrl: function() {
+		var url = "http://localhost:3000/";
 		var isNative = EnvUtils.isNative();
-		var url = isNative || (location.href.indexOf('http://localhost/') == -1) ? 'http://myturn.mobi/' : 'http://localhost:3000/';
+		var baseUrl = location.href.substring(0, location.href.lastIndexOf("/") + 1);
+		if (isNative || (baseUrl != "http://localhost/")) url = baseUrl;
 		return url;
 	}
 });
